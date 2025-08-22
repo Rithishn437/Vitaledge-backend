37f6322bf401cab334ef0e2ec8292e4ab4e64dd1
RithishN <nrithish20041011@gmail.com>
2025-08-22 10:49:07 +0530

    Fix port for Render

--- backend/app.py
+++ backend/app.py
@@ -236,4 +236,4 @@ def download_excel():
     return send_file(output, download_name='VitalEdge_Analysis.xlsx', as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
